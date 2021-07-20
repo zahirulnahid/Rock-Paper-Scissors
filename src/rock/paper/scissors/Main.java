3e0b37452f7af7d5807d5df8d3f081aa1cc659ac
zahirulnahid <zahirulnahid@gmail.com>
2021-07-20 13:45:27 +0600

    20Jul2021

---
+++ src/rock/paper/scissors/Main.java
@@ -0,0 +1,12 @@
+
+package rock.paper.scissors;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       userInterface ui = new userInterface();
+       ui.setVisible(true);
+    }
+    
+}
